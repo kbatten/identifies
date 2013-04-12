1695a3c50ee43296c859029db4db2cdfac981a61
Keith Batten <kbatten@gmail.com>
2013-04-11 22:41:06 -0700

    add initial chef files

---
+++ README.md
@@ -0,0 +1,13 @@
+# identifies cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: keith batten (kbatten@gmail.com)
---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             "identifies"
+maintainer       "keith batten"
+maintainer_email "kbatten@gmail.com"
+license          "MIT"
+description      "Installs/Configures identifies"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
