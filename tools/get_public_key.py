b78b0dfb44fe548fb54ddaa54dbb962b1b749199
Keith Batten <kbatten@gmail.com>
2013-04-14 14:17:07 -0700

    pylint-pep8 get_public_key.py

--- tools/get_public_key.py
+++ tools/get_public_key.py
@@ -1,10 +1,34 @@
 #!/usr/bin/env python
+'''
+converts a public key into the format browserid expects
+'''
 
 import sys
 import re
 
 
-if __name__ == '__main__':
+def key_firefox(keyfile):
+    '''
+    keyfile is a cut and paste from firefox public key info
+    '''
+    modulus_hex = ""
+    exponent = ""
+    for line in keyfile.readlines():
+        modulus_line = re.search('''^[0-9a-f]{2} [0-9a-f]{2}''', line.strip())
+        if modulus_line:
+            modulus_hex += line.strip()
+        exponent_line = re.search('''^[0-9]+$''', line.strip())
+        if exponent_line:
+            exponent += line.strip()
+    modulus_hex = "".join(modulus_hex.split())
+    modulus = int(modulus_hex, 16)
+    return modulus, exponent
+
+
+def main():
+    '''
+    main
+    '''
     if len(sys.argv) < 3:
         print '''
 ./get_public_key.py <option> <file>'
@@ -15,17 +39,11 @@ options:
         sys.exit(1)
 
     if sys.argv[1] == '-f':
-        # cut and paste from firefox public key info
-        modulus_hex = ""
-        exponent = ""
-        with open(sys.argv[2]) as f:
-            for line in f.readlines():
-                r = re.search('''^[0-9a-f]{2} [0-9a-f]{2}''', line.strip())
-                if r:
-                    modulus_hex += line.strip()
-                r = re.search('''^[0-9]+$''', line.strip())
-                if r:
-                    exponent += line.strip()
-        modulus_hex = "".join(modulus_hex.split())
-        modulus = int(modulus_hex, 16)
-        print modulus, exponent
+        with open(sys.argv[2]) as keyfile:
+            modulus, exponent = key_firefox(keyfile)
+        print "\"n\": \"%s\"" % modulus
+        print "\"e\": \"%s\"" % exponent
+
+
+if __name__ == '__main__':
+    main()
