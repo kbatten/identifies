8fa6bec023a0d85fa95827fda87053d061cdd23f
Keith Batten <kbatten@gmail.com>
2013-04-18 20:20:48 -0700

    add deploy script

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "kbatten@gmail.com"
 license          "MIT"
 description      "Installs/Configures identifies"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.1"
+version          "0.2.0"
 
 depends "apt"
 depends "nginx"
