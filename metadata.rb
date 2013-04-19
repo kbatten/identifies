05f3ab49e293153bd599eec7f39445f882b67d38
Keith Batten <kbatten@gmail.com>
2013-04-18 19:04:48 -0700

    address tls beast attack

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "kbatten@gmail.com"
 license          "MIT"
 description      "Installs/Configures identifies"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.1"
 
 depends "apt"
 depends "nginx"
